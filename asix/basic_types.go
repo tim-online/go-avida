c90e365ea4af9347b234096913e65b19e7dbeb99
Leon Bogaert <leon@tim-online.nl>
2017-08-22 15:19:16 +0200

    Added description and examples for types

--- asix/basic_types.go
+++ asix/basic_types.go
@@ -9,6 +9,8 @@ import (
 	"golang.org/x/text/currency"
 )
 
+// Amount with (maximum) 2 digits
+// Ex. 100.48 0.1 48
 type Amount float64
 
 func (a Amount) MarshalXML(e *xml.Encoder, start xml.StartElement) error {
@@ -21,6 +23,8 @@ func (a Amount) MarshalXML(e *xml.Encoder, start xml.StartElement) error {
 	return e.EncodeElement(formatted, start)
 }
 
+// Amount with (maximum) 5 digits
+// Ex. 100.4805 0.1 48
 type UnitAmount float64
 
 func (a UnitAmount) MarshalXML(e *xml.Encoder, start xml.StartElement) error {
@@ -33,22 +37,44 @@ func (a UnitAmount) MarshalXML(e *xml.Encoder, start xml.StartElement) error {
 	return e.EncodeElement(formatted, start)
 }
 
+// String with upper characters.
+// Valid characters: A-Z, 0-9, ÅÄÖ, -_
 type Code string
 
+// String with upper characters.
+// Valid characters: A-Z, 0-9, ÅÄÖ, -_
+// Max Length 10 characters
 type Code10 string
 
+// String with upper characters.
+// Valid characters: A-Z, 0-9, ÅÄÖ, -_
+// Max Length 20 characters
 type Code20 string
 
+// String with upper characters.
+// Valid characters: A-Z, 0-9, ÅÄÖ, -_
+// Max Length 30 characters
 type Code30 string
 
+// ISO 3166 Formatted Country Code
+// (http://www.iso.org/iso/country_codes/iso_3166_code_lists.htm)
 type CountryCode string
 
 // type CountryCode string
 // 	language.Region
 // }
 
+// Reference type of CreditNote.
+// Valid is 0-3.
+// 0 – Non connected credit
+// 1 - References a previous InvoiceNo
+// 2 - References a previous OrderNo
+// 3 - References a previous PackageNo
 type CreditRefType int
 
+// Currency Used in Invoice/CreditNote.
+// Valid format is 3 uppercase characters.
+// Should follow ISO 4217 (http://wikipedia.org/wiki/ISO_4217)
 type CurrencyType struct {
 	currency.Unit
 }
@@ -57,12 +83,16 @@ func (c CurrencyType) MarshalXML(e *xml.Encoder, start xml.StartElement) error {
 	return e.EncodeElement(c.String(), start)
 }
 
+// Not in use today.
 type InvoiceActionType int
 
+// Same as Code20. Cannot be empty.
 type NonEmptyCode20 string
 
+// Decimal entry
 type Percentage float64
 
+// Positive Decimal
 type PositiveAmount float64
 
 func (a PositiveAmount) MarshalXML(e *xml.Encoder, start xml.StartElement) error {
@@ -75,6 +105,8 @@ func (a PositiveAmount) MarshalXML(e *xml.Encoder, start xml.StartElement) error
 	return e.EncodeElement(formatted, start)
 }
 
+// Date in format YYYY-MM-DD. After 1800-01-01.
+// Ex. 2010-02-24
 type RequiredDate struct {
 	date.Date
 }
@@ -91,6 +123,7 @@ func (d RequiredDate) MarshalXMLAttr(name xml.Name) (xml.Attr, error) {
 	return xml.Attr{name, d.String()}, nil
 }
 
+// Same as RequiredDate, but blank values are allowed
 type Date struct {
 	date.Date
 }
@@ -102,22 +135,41 @@ func (d Date) MarshalXML(e *xml.Encoder, start xml.StartElement) error {
 	return e.EncodeElement(d.String(), start)
 }
 
+// Textstring with max length 10 characters.
 type Text10 string
 
+// Textstring with max length 20 characters.
 type Text20 string
 
+// Textstring with max length 30 characters.
 type Text30 string
 
+// Textstring with max length 50 characters.
 type Text50 string
 
+// Textstring with max length 80 characters.
 type Text80 string
 
+// Textstring with max length 150 characters.
 type Text150 string
 
+// How a document should be sent
+// 0 – no preference / use system setting
+// 1 – Email preferred (if available)
+// 2 – Mail Prioritaire (“A-Post” in Swedish)
+// 3 – Mail (normal) (“B-Post” in Swedish)
+// 4 – E-Invoice
+// 5 – Autogiro
 type DeliveryOptionType int
 
+// Save the ‘DeliveryOptionType’ to the system or not
+// 0 – Don’t save (default)
+// 1 – Save as preferred value for future documents
 type SaveAttribType int
 
+// Type of invoice account
+// 1 – Unique Account
+// 2 – Shared Account
 type AccountTypeEnum int
 
 type Decimal float64
--- asix/ledger.go
+++ asix/ledger.go
@@ -21,80 +21,273 @@ func NewLedger() *Ledger {
 }
 
 type Invoice struct {
+	// Client Number in Asitis Finance, ask your contact at the Finance company.
 	ClientNo NonEmptyCode20 `xml:"ClientNo,omitempty"`
-	// Action
-	CustLegalNo        Code20         `xml:"CustLegalNo,omitempty"`
-	CustNo             NonEmptyCode20 `xml:"CustNo,omitempty"`
-	Name               Text50         `xml:"Name,omitempty"`
-	Name2              Text50         `xml:"Name2,omitempty"`
-	Adress             Text30         `xml:"Adress,omitempty"`
-	Adress2            Text30         `xml:"Adress2,omitempty"`
-	PostCode           Text20         `xml:"PostCode,omitempty"`
-	City               Text30         `xml:"City,omitempty"`
-	CountryCode        CountryCode    `xml:"CountryCode,omitempty"`
-	InvoiceNo          NonEmptyCode20 `xml:"InvoiceNo,omitempty"`
-	InvoiceDate        RequiredDate   `xml:"InvoiceDate,omitempty"`
-	InvoiceDueDate     RequiredDate   `xml:"InvoiceDueDate,omitempty"`
-	Amount             PositiveAmount `xml:"Amount,omitempty"`
-	VATAmount          PositiveAmount `xml:"VATAmount,omitempty"`
-	Currency           CurrencyType   `xml:"Currency,omitempty"`
-	OrderDate          RequiredDate   `xml:"OrderDate,omitempty"`
-	DeliveryDate       RequiredDate   `xml:"DeliveryDate,omitempty"`
-	PaymentRefNo       Code30         `xml:"PaymentRefNo,omitempty"`
-	OrderNo            Code20         `xml:"OrderNo,omitempty"`
-	PackageNo          Text30         `xml:"PackageNo,omitempty"`
-	PartialPaymentCode Code10         `xml:"PartialPaymentCode,omitempty"`
-	PostProcessingCode Code10         `xml:"PostProcessingCode,omitempty"`
-	InvoiceAccount     InvoiceAccount `xml:"InvoiceAccount,omitempty"`
-	Lines              Lines          `xml:"Line,omitempty"`
+
+	// (not used at this time)
+	// Action             interface{}    `xml:"Action,omitempty"`
+
+	// Customers Legal No.
+	// 1234561234
+	CustLegalNo Code20 `xml:"CustLegalNo,omitempty"`
+
+	// Customers No.
+	// C00002
+	CustNo NonEmptyCode20 `xml:"CustNo,omitempty"`
+
+	// Customers Name
+	// Albert Nobel
+	Name Text50 `xml:"Name,omitempty"`
+
+	// Customers Name, second line.
+	// C/O Name
+	Name2 Text50 `xml:"Name2,omitempty"`
+
+	// Customers postal Adress.
+	// Dynamic road 2
+	Adress Text30 `xml:"Adress,omitempty"`
+
+	// Customers postal Adress, second line.
+	Adress2 Text30 `xml:"Adress2,omitempty"`
+
+	// Customers Postal code.
+	// 54130
+	PostCode Text20 `xml:"PostCode,omitempty"`
+
+	// Customers postal city.
+	// Skövde
+	City Text30 `xml:"City,omitempty"`
+
+	// Customers Country code.
+	// SE
+	CountryCode CountryCode `xml:"CountryCode,omitempty"` // description: Customers Country code.
+
+	// Invoice No.
+	// F002110
+	InvoiceNo NonEmptyCode20 `xml:"InvoiceNo,omitempty"`
+
+	// Invoice Date
+	// 2010-02-14
+	InvoiceDate RequiredDate `xml:"InvoiceDate,omitempty"`
+
+	// Due Date for the Invoice
+	// 2010-03-14
+	InvoiceDueDate RequiredDate `xml:"InvoiceDueDate,omitempty"` // description: Due Date for the Invoice
+
+	// Total Sum of Invoice. Always positive.
+	// 34521
+	Amount PositiveAmount `xml:"Amount,omitempty"` // description:
+
+	// Total VAT Amount. Always positive.
+	// 8630.25
+	VATAmount PositiveAmount `xml:"VATAmount,omitempty"` // description:
+
+	// Currency of Invoice
+	// SEK
+	Currency CurrencyType `xml:"Currency,omitempty"` // description:
+
+	// Date when Order was made.
+	// 2010-02-10
+	OrderDate RequiredDate `xml:"OrderDate,omitempty"` // description:
+
+	// Date then Order was delivered.
+	// 2010-02-14
+	DeliveryDate RequiredDate `xml:"DeliveryDate,omitempty"` // description:
+
+	// Payment reference number or code.
+	// 0021108
+	PaymentRefNo Code30 `xml:"PaymentRefNo,omitempty"` // description:
+
+	// Order Number.
+	// 002110
+	OrderNo Code20 `xml:"OrderNo,omitempty"` // description:
+
+	// Package / tracking number for delivery.
+	PackageNo Text30 `xml:"PackageNo,omitempty"` // description:
+
+	// Partial Payment Code, when payment is partial.
+	PartialPaymentCode Code10 `xml:"PartialPaymentCode,omitempty"` // description:
+
+	// Identifier for debt. collection processing.
+	// INV02
+	PostProcessingCode Code10 `xml:"PostProcessingCode,omitempty"` // description:
+
+	// Indicates if the invoice shall be posted against an account or not
+	InvoiceAccount InvoiceAccount `xml:"InvoiceAccount,omitempty"` // description:
+
+	// See Lines
+	Lines Lines `xml:"Line,omitempty"` // description:
 
 	// Optional Fields
-	OurRef             Text30      `xml:"OurRef,omitempty"`
-	YourRef            Text30      `xml:"YourRef,omitempty"`
-	CustVATNo          Text30      `xml:"CustVATNo,omitempty"`
-	Email              Text80      `xml:"Email,omitempty"`
-	PhoneNo            Text30      `xml:"PhoneNo,omitempty"`
-	DeliveryOption     int         `xml:"DeliveryOption,omitempty"`
-	GLN                Code20      `xml:"GLN,omitempty"`
-	ShipName           Text50      `xml:"ShipName,omitempty"`
-	ShipName2          Text50      `xml:"ShipName2,omitempty"`
-	ShipAdress         Text30      `xml:"ShipAdress,omitempty"`
-	ShipAdress2        Text30      `xml:"ShipAdress2,omitempty"`
-	ShipPostCode       Text20      `xml:"ShipPostCode,omitempty"`
-	ShipCity           Text30      `xml:"ShipCity,omitempty"`
-	ShipCountryCode    CountryCode `xml:"ShipCountryCode,omitempty"`
-	Language           Code10      `xml:"Language,omitempty"`
-	PaymentRequestType int         `xml:"PaymentRequestType,omitempty"`
-	PaymentRequestId   Text30      `xml:"PaymentRequestId,omitempty"`
-	SourceSystem       Code20      `xml:"SourceSystem,omitempty"`
+
+	// "Our Reference" on the document
+	// Salesperson Xy
+	OurRef Text30 `xml:"OurRef,omitempty"` // description:
+
+	// "Your Reference" on the document
+	// John Doe
+	YourRef Text30 `xml:"YourRef,omitempty"` // description:
+
+	// The Customers VAT Reg. No
+	// SE554433221101
+	CustVATNo Text30 `xml:"CustVATNo,omitempty"` // description:
+
+	// The Customers Email
+	// john@some.where
+	Email Text80 `xml:"Email,omitempty"` // description:
+
+	// The Customers Phone Number
+	// 016 13 75 20
+	PhoneNo Text30 `xml:"PhoneNo,omitempty"` // description:
+
+	// How to deliver the document
+	// 1
+	DeliveryOption int `xml:"DeliveryOption,omitempty"` // description:
+
+	// Global Location Number
+	// 3322115544621
+	GLN Code20 `xml:"GLN,omitempty"` // description:
+
+	// Shipment Name on document
+	ShipName Text50 `xml:"ShipName,omitempty"` // description:
+
+	// Shipment Name, second line.
+	// C/O Name
+	ShipName2 Text50 `xml:"ShipName2,omitempty"` // description:
+
+	// Shipment postal Adress.
+	// Dynamic road 2
+	ShipAdress Text30 `xml:"ShipAdress,omitempty"` // description:
+
+	// Shipment postal Adress, second line.
+	ShipAdress2 Text30 `xml:"ShipAdress2,omitempty"` // description:
+
+	// Shipment Postal code.
+	// 54130
+	ShipPostCode Text20 `xml:"ShipPostCode,omitempty"` // description:
+
+	// Shipment postal city.
+	// Skövde
+	ShipCity Text30 `xml:"ShipCity,omitempty"` // description:
+
+	// Shipment Country code.
+	// SE
+	ShipCountryCode CountryCode `xml:"ShipCountryCode,omitempty"` // description:
+
+	// A languagecode in the receiving system, or a decimal value from this list (1033 for English, 1053 for Swedish)
+	// SV
+	// 1053
+	Language Code10 `xml:"Language,omitempty"` // description:
+
+	// 0 = No payment request
+	// 1 = Autogiro
+	// 0
+	PaymentRequestType int `xml:"PaymentRequestType,omitempty"` // description:
+
+	// Id to be used with the selected payment request type.  For Autogiro this is 'betalarnr'
+	// 438294
+	PaymentRequestId Text30 `xml:"PaymentRequestId,omitempty"` // description:
+
+	// Source System ID. Indicates that the client no. specified will be converted to the internal client no. used in Asitis Finance.  The Source System ID and mapping has to be entered in the application.
+	// EXTERNAL
+	SourceSystem Code20 `xml:"SourceSystem,omitempty"` // description:
 
 	// Only for invoice
-	Attachments Attachments `xml:"Attachment,omitempty"`
+	Attachments Attachments `xml:"Attachment,omitempty"` // description:
 }
 
 type Credit struct {
-	ClientNo      NonEmptyCode20 `xml:"ClientNo,omitempty"`
-	CustLegalNo   Code20         `xml:"CustLegalNo,omitempty"`
-	CustNo        NonEmptyCode20 `xml:"CustNo,omitempty"`
-	Name          Text50         `xml:"Name,omitempty"`
-	Name2         Text50         `xml:"Name2,omitempty"`
-	Adress        Text30         `xml:"Adress,omitempty"`
-	Adress2       Text30         `xml:"Adress2,omitempty"`
-	PostCode      Text20         `xml:"PostCode,omitempty"`
-	City          Text30         `xml:"City,omitempty"`
-	CountryCode   CountryCode    `xml:"CountryCode,omitempty"`
-	CreditNo      NonEmptyCode20 `xml:"CreditNo,omitempty"`
-	CreditDate    RequiredDate   `xml:"CreditDate,omitempty"`
-	CreditDueDate RequiredDate   `xml:"CreditDueDate,omitempty"`
-	CreditRefType CreditRefType  `xml:"CreditRefType,omitempty"`
-	Amount        PositiveAmount `xml:"Amount,omitempty"`
-	VATAmount     PositiveAmount `xml:"VATAmount,omitempty"`
-	Currency      CurrencyType   `xml:"Currency,omitempty"`
-	OrderDate     RequiredDate   `xml:"OrderDate,omitempty"`
-	PaymentRefNo  Code30         `xml:"PaymentRefNo,omitempty"`
-	OrderNo       Code20         `xml:"OrderNo,omitempty"`
-	PackageNo     Text30         `xml:"PackageNo,omitempty"`
-	Lines         Lines          `xml:"Line,omitempty"`
+	// Client Number in Asitis Finance, ask your contact at the Finance company.
+	// 0050, C001
+	ClientNo NonEmptyCode20 `xml:"ClientNo,omitempty"`
+
+	// Customers Legal No.
+	// 1234561234
+	CustLegalNo Code20 `xml:"CustLegalNo,omitempty"`
+
+	// Customers No.
+	// C00002
+	CustNo NonEmptyCode20 `xml:"CustNo,omitempty"`
+
+	// Customers Name
+	// Albert Nobel
+	Name Text50 `xml:"Name,omitempty"`
+
+	// Customers Name, second line.
+	// C/O Name
+	Name2 Text50 `xml:"Name2,omitempty"`
+
+	// Customers postal Adress.
+	// Dynamic road 2
+	Adress Text30 `xml:"Adress,omitempty"`
+
+	// Customers postal Adress, second line.
+	Adress2 Text30 `xml:"Adress2,omitempty"`
+
+	// Customers Postal code.
+	// 54130
+	PostCode Text20 `xml:"PostCode,omitempty"`
+
+	// Customers postal city.
+	// Skövde
+	City Text30 `xml:"City,omitempty"`
+
+	// Customers Country code.
+	// SE
+	CountryCode CountryCode `xml:"CountryCode,omitempty"`
+
+	// Credit Note No.
+	// F002110
+	CreditNo NonEmptyCode20 `xml:"CreditNo,omitempty"`
+
+	// Credit Note Date
+	// 2010-02-14
+	CreditDate RequiredDate `xml:"CreditDate,omitempty"`
+
+	// Due Date for the Credit
+	// 2010-03-14
+	CreditDueDate RequiredDate `xml:"CreditDueDate,omitempty"`
+
+	// Ref type
+	// 0
+	CreditRefType CreditRefType `xml:"CreditRefType,omitempty"`
+
+	// Ref Identifier
+	// 12354
+	CreditRefNo Code30 `xml:"CreditRefNo,omitempty"`
+
+	// Total Sum of Invoice. Always positive.
+	// 34521
+	Amount PositiveAmount `xml:"Amount,omitempty"`
+
+	// Total VAT Amount. Always positive.
+	// 8630.25
+	VATAmount PositiveAmount `xml:"VATAmount,omitempty"`
+
+	// Currency of Invoice
+	// SEK
+	Currency CurrencyType `xml:"Currency,omitempty"`
+
+	// Date when Order was made.
+	// 2010-02-10
+	OrderDate RequiredDate `xml:"OrderDate,omitempty"`
+
+	// Date then Order was delivered.
+	// 2010-02-14
+	DeliveryDate RequiredDate `xml:"RequiredDate,omitempty"`
+
+	// Payment reference number or code.
+	PaymentRefNo Code30 `xml:"PaymentRefNo,omitempty"`
+
+	// Order Number.
+	// 002110
+	OrderNo Code20 `xml:"OrderNo,omitempty"`
+
+	// Package / tracking number for delivery.
+	PackageNo Text30 `xml:"PackageNo,omitempty"`
+
+	// See Lines
+	Lines Lines `xml:"Line,omitempty"`
 }
 
 type InvoiceAccount struct {
@@ -118,23 +311,63 @@ type Lines []Line
 // Any numeric field can be negative, as long as the formulas are still valid.
 // The sum of all ‘LineAmountInclVAT’ must always be positive.
 type Line struct {
-	ItemNo            Text20     `xml:"ItemNo"` // description: Item No, Article No. // example: 001220
-	Description       Text80     `xml:"Description,omitempty"`
-	Description2      Text80     `xml:"Description2,omitempty"`
-	UnitOfMeasure     Text10     `xml:"UnitOfMeasure,omitempty"`
-	Quantity          Decimal    `xml:"Quantity,omitempty"`
-	UnitPrice         UnitAmount `xml:"UnitPrice,omitempty"`
-	VATPct            Percentage `xml:"VATPct,omitempty"`
-	VATAmount         Amount     `xml:"VATAmount,omitempty"`
-	DiscountPct       Percentage `xml:"DiscountPct,omitempty"`
-	LineAmountExclVAT Amount     `xml:"LineAmountExclVAT,omitempty"`
-	LineAmountInclVAT Amount     `xml:"LineAmountInclVAT,omitempty"`
+	// Item No, Article No.
+	// 001220
+	ItemNo Text20 `xml:"ItemNo"`
+
+	// Description of Item.
+	// Black Pencil
+	Description Text80 `xml:"Description,omitempty"`
+
+	// Description of Item, Second line.
+	// With red line.
+	Description2 Text80 `xml:"Description2,omitempty"`
+
+	// Measurement Unit
+	// M, kg, Hour
+	UnitOfMeasure Text10 `xml:"UnitOfMeasure,omitempty"`
+
+	// Quantity
+	// 10
+	Quantity Decimal `xml:"Quantity,omitempty"`
+
+	// The price for one item. Excluding VAT.
+	// 2.9805
+	UnitPrice UnitAmount `xml:"UnitPrice,omitempty"`
+
+	// VAT Percent for item
+	// 25
+	VATPct Percentage `xml:"VATPct,omitempty"`
+
+	// VAT Amount for all Items.  7.45 Quantity * UnitPrice * (VATPct/100)
+	// 7.45
+	VATAmount Amount `xml:"VATAmount,omitempty"`
+
+	// Discount in percent.
+	// 0
+	DiscountPct Percentage `xml:"DiscountPct,omitempty"`
+
+	// Line Amount, Quantity * UnitPrice
+	// 29.8
+	LineAmountExclVAT Amount `xml:"LineAmountExclVAT,omitempty"`
+
+	// LineAmountExclVAT + VATAmount
+	// 37.25
+	LineAmountInclVAT Amount `xml:"LineAmountInclVAT,omitempty"`
 }
 
 type Attachments []Attachment
 
 type Attachment struct {
-	Name  Text50  `xml:"Name,omitempty"`
-	Pages Text150 `xml:"Pages,omitempty"`
-	Realm int     `xml:"Realm,omitempty"`
+	// Namespace for attachment
+	// Company A or Client A
+	Realm Text50 `xml:"Realm,omitempty"`
+
+	// File Name
+	// InvoieNo_1_Attachment1.pdf
+	Name Text150 `xml:"Name,omitempty"`
+
+	// Number of pages of attachment
+	// 5
+	Pages int `xml:"Pages,omitempty"`
 }
