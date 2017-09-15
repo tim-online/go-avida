df0347f3de0d602cbd6eee12946e3ab9f8fc21bf
Leon Bogaert <leon@tim-online.nl>
2017-09-15 11:21:03 +0200

    Restuctured types based on AD-ASIXSpecification-020817-1009-44_2.pdf

--- asix/ledger.go
+++ asix/ledger.go
@@ -24,9 +24,6 @@ type Invoice struct {
 	// Client Number in Asitis Finance, ask your contact at the Finance company.
 	ClientNo NonEmptyCode20 `xml:"ClientNo,omitempty"`
 
-	// (not used at this time)
-	// Action             interface{}    `xml:"Action,omitempty"`
-
 	// Customers Legal No.
 	// 1234561234
 	CustLegalNo Code20 `xml:"CustLegalNo"`
@@ -38,10 +35,6 @@ type Invoice struct {
 	// Customers Name
 	// Albert Nobel
 	Name Text50 `xml:"Name,omitempty"`
-
-	// Customers Name, second line.
-	// C/O Name
-	Name2 Text50 `xml:"Name2,omitempty"`
 
 	// Customers postal Adress.
 	// Dynamic road 2
@@ -57,10 +50,6 @@ type Invoice struct {
 	// Customers postal city.
 	// Skövde
 	City Text30 `xml:"City"`
-
-	// Customers Country code.
-	// SE
-	CountryCode CountryCode `xml:"CountryCode"` // description: Customers Country code.
 
 	// Invoice No.
 	// F002110
@@ -86,42 +75,17 @@ type Invoice struct {
 	// SEK
 	Currency CurrencyType `xml:"Currency,omitempty"` // description:
 
-	// Date when Order was made.
-	// 2010-02-10
-	OrderDate RequiredDate `xml:"OrderDate"` // description:
-
-	// Date then Order was delivered.
-	// 2010-02-14
-	DeliveryDate RequiredDate `xml:"DeliveryDate"` // description:
-
 	// Payment reference number or code.
 	// 0021108
 	PaymentRefNo Code30 `xml:"PaymentRefNo,omitempty"` // description:
 
-	// Order Number.
-	// 002110
-	OrderNo Code20 `xml:"OrderNo"` // description:
-
-	// Package / tracking number for delivery.
-	PackageNo Text30 `xml:"PackageNo,omitempty"` // description:
-
-	// Partial Payment Code, when payment is partial.
-	PartialPaymentCode Code10 `xml:"PartialPaymentCode,omitempty"` // description:
-
-	// Identifier for debt. collection processing.
-	// INV02
-	PostProcessingCode Code10 `xml:"PostProcessingCode,omitempty"` // description:
-
-	// Indicates if the invoice shall be posted against an account or not
-	InvoiceAccount InvoiceAccount `xml:"InvoiceAccount,omitempty"` // description:
+	// "Our Reference" on the document
+	// Salesperson Xy
+	OurRef Text30 `xml:"OurRef,omitempty"` // description:
 
 	// "Your Reference" on the document
 	// John Doe
 	YourRef Text30 `xml:"YourRef,omitempty"` // description:
-
-	// The Customers VAT Reg. No
-	// SE554433221101
-	CustVATNo Text30 `xml:"CustVATNo,omitempty"` // description:
 
 	// The Customers Email
 	// john@some.where
@@ -129,71 +93,6 @@ type Invoice struct {
 
 	// See Lines
 	Lines Lines `xml:"Line,omitempty"` // description:
-
-	// Optional Fields
-
-	// "Our Reference" on the document
-	// Salesperson Xy
-	OurRef Text30 `xml:"OurRef,omitempty"` // description:
-
-	// The Customers Phone Number
-	// 016 13 75 20
-	PhoneNo Text30 `xml:"PhoneNo,omitempty"` // description:
-
-	// How to deliver the document
-	// 1
-	DeliveryOption int `xml:"DeliveryOption,omitempty"` // description:
-
-	// Global Location Number
-	// 3322115544621
-	GLN Code20 `xml:"GLN,omitempty"` // description:
-
-	// Shipment Name on document
-	ShipName Text50 `xml:"ShipName,omitempty"` // description:
-
-	// Shipment Name, second line.
-	// C/O Name
-	ShipName2 Text50 `xml:"ShipName2,omitempty"` // description:
-
-	// Shipment postal Adress.
-	// Dynamic road 2
-	ShipAdress Text30 `xml:"ShipAdress,omitempty"` // description:
-
-	// Shipment postal Adress, second line.
-	ShipAdress2 Text30 `xml:"ShipAdress2,omitempty"` // description:
-
-	// Shipment Postal code.
-	// 54130
-	ShipPostCode Text20 `xml:"ShipPostCode,omitempty"` // description:
-
-	// Shipment postal city.
-	// Skövde
-	ShipCity Text30 `xml:"ShipCity,omitempty"` // description:
-
-	// Shipment Country code.
-	// SE
-	ShipCountryCode CountryCode `xml:"ShipCountryCode,omitempty"` // description:
-
-	// A languagecode in the receiving system, or a decimal value from this list (1033 for English, 1053 for Swedish)
-	// SV
-	// 1053
-	Language Code10 `xml:"Language,omitempty"` // description:
-
-	// 0 = No payment request
-	// 1 = Autogiro
-	// 0
-	PaymentRequestType int `xml:"PaymentRequestType,omitempty"` // description:
-
-	// Id to be used with the selected payment request type.  For Autogiro this is 'betalarnr'
-	// 438294
-	PaymentRequestId Text30 `xml:"PaymentRequestId,omitempty"` // description:
-
-	// Source System ID. Indicates that the client no. specified will be converted to the internal client no. used in Asitis Finance.  The Source System ID and mapping has to be entered in the application.
-	// EXTERNAL
-	SourceSystem Code20 `xml:"SourceSystem,omitempty"` // description:
-
-	// Only for invoice
-	Attachments Attachments `xml:"Attachment,omitempty"` // description:
 }
 
 type Credit struct {
@@ -213,10 +112,6 @@ type Credit struct {
 	// Albert Nobel
 	Name Text50 `xml:"Name,omitempty"`
 
-	// Customers Name, second line.
-	// C/O Name
-	Name2 Text50 `xml:"Name2,omitempty"`
-
 	// Customers postal Adress.
 	// Dynamic road 2
 	Adress Text30 `xml:"Adress"`
@@ -231,10 +126,6 @@ type Credit struct {
 	// Customers postal city.
 	// Skövde
 	City Text30 `xml:"City"`
-
-	// Customers Country code.
-	// SE
-	CountryCode CountryCode `xml:"CountryCode"`
 
 	// Credit Note No.
 	// F002110
@@ -268,23 +159,17 @@ type Credit struct {
 	// SEK
 	Currency CurrencyType `xml:"Currency,omitempty"`
 
-	// Date when Order was made.
-	// 2010-02-10
-	OrderDate RequiredDate `xml:"OrderDate"`
+	// "Our Reference" on the document
+	// Salesperson Xy
+	OurRef Text30 `xml:"OurRef,omitempty"` // description:
 
-	// Date then Order was delivered.
-	// 2010-02-14
-	DeliveryDate RequiredDate `xml:"DeliveryDate"`
+	// "Your Reference" on the document
+	// John Doe
+	YourRef Text30 `xml:"YourRef,omitempty"` // description:
 
-	// Payment reference number or code.
-	PaymentRefNo Code30 `xml:"PaymentRefNo,omitempty"`
-
-	// Order Number.
-	// 002110
-	OrderNo Code20 `xml:"OrderNo"`
-
-	// Package / tracking number for delivery.
-	PackageNo Text30 `xml:"PackageNo,omitempty"`
+	// The Customers Email
+	// john@some.where
+	Email Text80 `xml:"Email,omitempty"` // description:
 
 	// See Lines
 	Lines Lines `xml:"Line,omitempty"`
@@ -354,20 +239,4 @@ type Line struct {
 	// LineAmountExclVAT + VATAmount
 	// 37.25
 	LineAmountInclVAT Amount `xml:"LineAmountInclVAT,omitempty"`
-}
-
-type Attachments []Attachment
-
-type Attachment struct {
-	// Namespace for attachment
-	// Company A or Client A
-	Realm Text50 `xml:"Realm,omitempty"`
-
-	// File Name
-	// InvoieNo_1_Attachment1.pdf
-	Name Text150 `xml:"Name,omitempty"`
-
-	// Number of pages of attachment
-	// 5
-	Pages int `xml:"Pages,omitempty"`
 }
